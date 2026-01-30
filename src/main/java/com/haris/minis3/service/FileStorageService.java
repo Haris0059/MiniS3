2383400e095323bdd603e3613ba6e384e71ccfb2
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2026-01-30 21:02:06 +0100

    feat: add getAllFiles method to FileStorageService interface

--- src/main/java/com/haris/minis3/service/FileStorageService.java
+++ src/main/java/com/haris/minis3/service/FileStorageService.java
@@ -4,7 +4,10 @@ import com.haris.minis3.entity.FileMetadata;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface FileStorageService {
     FileMetadata storeFile(MultipartFile file) throws IOException;
+
+    List<FileMetadata> getAllFiles();
 }
