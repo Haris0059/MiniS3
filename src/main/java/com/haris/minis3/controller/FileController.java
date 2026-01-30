4e0f829a1d02e14e06f9fa462b0451ce4f3607e0
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2026-01-30 21:01:54 +0100

    feat: add GET /api/v1/files endpoint to list all files

--- src/main/java/com/haris/minis3/controller/FileController.java
+++ src/main/java/com/haris/minis3/controller/FileController.java
@@ -3,13 +3,11 @@ package com.haris.minis3.controller;
 import com.haris.minis3.entity.FileMetadata;
 import com.haris.minis3.service.FileStorageService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/files")
@@ -24,5 +22,10 @@ public class FileController {
     @PostMapping
     public ResponseEntity<FileMetadata> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         return ResponseEntity.ok(fileStorageService.storeFile(file));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FileMetadata>> listFiles() {
+        return ResponseEntity.ok(fileStorageService.getAllFiles());
     }
 }
